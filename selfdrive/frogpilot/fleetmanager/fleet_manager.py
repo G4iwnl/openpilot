39f1bcf77b497b213bc5b31d4b53ca86a386dd9f
G4iwnl <ytghast@daum.net>
2025-03-24 23:11:40 +0900

    Revert "1"
    This reverts commit 9472de6136e4859f40e9eb03efd98cc6999bb958.

--- selfdrive/frogpilot/fleetmanager/fleet_manager.py
+++ selfdrive/frogpilot/fleetmanager/fleet_manager.py
@@ -261,14 +261,12 @@ def route(route):
     query_segment = (str(request.query_string).split(","))[0][2:]
     query_type = (str(request.query_string).split(","))[1][:-1]
 
-  segments = list(fleet.segments_in_route(route)) 
-  return render_template(
-        "route.html",
-        route=route,
-        query_type=query_type,
-        segments=segments,
-        query_segment=query_segment
-    )
+  links = ""
+  segments = ""
+  for segment in fleet.segments_in_route(route):
+    links += "<a href='"+route+"?"+segment.split("--")[2]+","+query_type+"'>"+segment+"</a><br>"
+    segments += "'"+segment+"',"
+  return render_template("route.html", route=route, query_type=query_type, links=links, segments=segments, query_segment=query_segment)
 
 
 @app.route("/footage/")
