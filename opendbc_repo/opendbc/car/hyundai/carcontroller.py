1e6dffe369df6d4e984521ce45e630b0abde9c5c
ajouatom <ajouatom@gmail.com>
2025-03-31 18:13:42 +0900

    fix angle curvature...

--- opendbc_repo/opendbc/car/hyundai/carcontroller.py
+++ opendbc_repo/opendbc/car/hyundai/carcontroller.py
@@ -138,7 +138,7 @@ class CarController(CarControllerBase):
       self.lkas_max_torque = max(self.lkas_max_torque - 20, 5)
     else:
       #target_torque = np.interp(CS.out.vEgo, [0, 4], [40, self.angle_max_torque])
-      target_torque = np.interp(abs(actuators.curvature), [0.0, 0.03, 0.06], [0.5 * self.angle_max_torque, 0.75 * self.angle_max_torque, self.angle_max_torque])
+      target_torque = np.interp(abs(actuators.curvature), [0.0, 0.003, 0.006], [0.5 * self.angle_max_torque, 0.75 * self.angle_max_torque, self.angle_max_torque])
       #if abs(self.apply_angle_last) < 1.0:
       #  torque_ratio = np.interp(abs(self.apply_angle_last), [0, 1.0], [0.5, 1.0])
       #  target_torque = min(target_torque, self.angle_max_torque * torque_ratio)
