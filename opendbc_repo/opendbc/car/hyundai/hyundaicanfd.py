8d407170b176bc751ddac7fe098ac92421c17b84
ajouatom <ajouatom@gmail.com>
2025-07-18 09:56:50 +0900

    fix. ccnc

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -464,7 +464,7 @@ def create_ccnc_messages(CP, packer, CAN, frame, CC, CS, hud_control, disp_angle
         if CS.adrv_info_161 is not None:
           main_enabled = CS.out.cruiseState.available
           cruise_enabled = CC.enabled
-          lat_enabled = CS.latEnabled
+          lat_enabled = CS.out.latEnabled
           lat_active = CC.latActive
           nav_active = hud_control.activeCarrot > 1
 
