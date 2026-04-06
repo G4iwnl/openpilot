54b951fc32027e8459772a472d7865b120e2844b
Rusiperso <127375970+Rusiperso@users.noreply.github.com>
2026-04-06 13:57:30 +0900

    Update params_keys.h

--- common/params_keys.h
+++ common/params_keys.h
@@ -142,7 +142,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"GMapKey", {PERSISTENT, STRING}},
     {"SearchInput", {PERSISTENT, INT}},
 
-    {"CarSelected3", {PERSISTENT, STRING, "Kia Sorento Hybrid 2021-23"}},
+    {"CarSelected3", {PERSISTENT, STRING, "MOCK"}},
     {"SupportedCars", {PERSISTENT, STRING}},
     {"SupportedCars_gm", {PERSISTENT, STRING}},
     {"ShowDebugUI", {PERSISTENT, INT, "1"}},
